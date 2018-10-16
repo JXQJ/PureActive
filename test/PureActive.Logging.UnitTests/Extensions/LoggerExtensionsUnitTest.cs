c91f0edc1efa92a209c7843774e2a8149af1455e
Steve Bush <stevebu@bushchang.com>
2018-10-16 13:03:34 -0700

    LoggerExtensions UnitTests

---
+++ test/PureActive.Logging.UnitTests/Extensions/LoggerExtensionsUnitTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace PureActive.Logging.UnitTests.Extensions
+{
+    public class LoggerExtensionsUnitTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}

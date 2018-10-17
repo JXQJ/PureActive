073cd4a39270e32e7f88efa0427e5a2b798d341f
Steve Bush <stevebu@bushchang.com>
2018-10-17 00:20:05 -0700

    Simple logger extensions

--- src/PureActive.Logging/Extensions/LoggerExtensions.cs
+++ src/PureActive.Logging/Extensions/LoggerExtensions.cs
@@ -1,7 +1,13 @@
-﻿namespace PureActive.Logging.Extensions
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace PureActive.Logging.Extensions
 {
     public static class LoggerExtensions
     {
-
+        public static void AddTime(this ILogger logger, string msg)
+        {
+            logger.Log(LogLevel.Debug, $"[{DateTimeOffset.Now}] {msg}");
+        }
     }
 }

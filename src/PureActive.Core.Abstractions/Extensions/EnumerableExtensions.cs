9955faa33cf30e64419bd9e5965c0b4b36878a51
Steve Bush <stevebu@bushchang.com>
2018-10-20 12:28:15 -0700

    Added projects for PureActive.Core

---
+++ src/PureActive.Core.Abstractions/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PureActive.Core.Abstractions.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static int MaxStringLength<T>(this IEnumerable<T> enumerable)
+        {
+            return enumerable.Select(item => item.ToString().Length).Concat(new[] { 0 }).Max();
+        }
+    }
+}

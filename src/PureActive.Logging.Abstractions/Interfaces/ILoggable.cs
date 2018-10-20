c8747dacd6a37e228bef4d234e58d5d9f482e3e0
Steve Bush <stevebu@bushchang.com>
2018-10-20 09:38:38 -0700

    PureActive Logging Abstractions
    Consolidation of PureActive csproj files

---
+++ src/PureActive.Logging.Abstractions/Interfaces/ILoggable.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace PureActive.Logging.Abstractions.Interfaces
+{
+    public interface ILoggable
+    {
+
+        string ToString(LogLevel logLevel);
+
+        string ToString(LogLevel logLevel, LoggableFormat loggableFormat);
+    }
+}
--- src/PureActive.Logging.Extensions/LoggerExtensions.cs
+++ src/PureActive.Logging.Extensions/LoggerExtensions.cs
@@ -5,9 +5,7 @@ namespace PureActive.Logging.Extensions
 {
     public static class LoggerExtensions
     {
-        public static void AddTime(this ILogger logger, string msg)
-        {
-            logger.Log(LogLevel.Debug, $"[{DateTimeOffset.Now}] {msg}");
-        }
+      
+
     }
 }

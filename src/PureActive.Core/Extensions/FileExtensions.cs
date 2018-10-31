da3225a60b374fe23810c13c7c1393ab93e5182f
Steve Bush <stevebu@bushchang.com>
2018-10-31 10:13:38 -0700

    Added Unit Tests for Log Rolling File

---
+++ src/PureActive.Core/Extensions/FileExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PureActive.Core.Extensions
+{
+    public static class FileExtensions
+    {
+
+        public static string GetRandomFileName(string prefix, string ext)
+        {
+            return $"{prefix}{Guid.NewGuid().ToStringNoDashes()}{ext}";
+        }
+
+    }
+}
--- test/PureActive.Logger.Provider.Serilog.UnitTests/SerilogProviderUnitTests.cs
+++ test/PureActive.Logger.Provider.Serilog.UnitTests/SerilogProviderUnitTests.cs
@@ -1,7 +1,11 @@
+using System.IO;
+using FluentAssertions;
 using Microsoft.Extensions.Logging;
+using PureActive.Core.Extensions;
 using PureActive.Core.System;
 using PureActive.Logger.Provider.Serilog.Configuration;
 using PureActive.Logger.Provider.Serilog.Settings;
+using PureActive.Logging.Abstractions.Interfaces;
 using PureActive.Logging.Abstractions.Types;
 using Serilog.Events;
 using Xunit;
@@ -10,10 +14,34 @@ namespace PureActive.Logger.Provider.Serilog.UnitTests
 {
     public class SerilogProviderUnitTests
     {
+        private void AssertLogFileEntry(IPureLoggerSettings loggerSettings, LogLevel logLevel, string msg, string logFileName)
+        {
+            string partialName = Path.GetFileNameWithoutExtension(logFileName);
+            DirectoryInfo hdDirectoryInWhichToSearch = new DirectoryInfo(loggerSettings.LogFolderPath);
+            FileInfo[] filesInDir = hdDirectoryInWhichToSearch.GetFiles("*" + partialName + "*.*");
+
+            foreach (FileInfo foundFile in filesInDir)
+            {
+                if (foundFile.Name.StartsWith(partialName))
+                {
+                    if (File.Exists(foundFile.FullName))
+                    {
+                        using (var sr = new StreamReader(foundFile.FullName))
+                        {
+                            var logContents = sr.ReadToEnd();
+
+                            logContents.Should().EndWith($"{msg}\r\n");
+                            logContents.Should().Contain($"[{logLevel}]");
+                        }
+                    }
+                }
+            }
+        }
+
         [Fact]
         public void SerilogProvider_CreateLogger_AppConsoleFile()
         {
-            var logFileName = "SerilogProviderUnitTests.log";
+            var logFileName = FileExtensions.GetRandomFileName("", ".log");
             var fileSystem = new FileSystem(typeof(SerilogProviderUnitTests));
 
             var loggerSettings = new SerilogLoggerSettings(fileSystem, LogEventLevel.Debug, LoggingOutputFlags.AppConsoleFile);
@@ -24,7 +52,13 @@ namespace PureActive.Logger.Provider.Serilog.UnitTests
 
             var logger = loggerFactory.CreateLogger<SerilogProviderUnitTests>();
 
-            logger.LogDebug("Test");
+            var msg = "Test";
+            logger.LogDebug(msg);
+
+            // Dispose Logger Factory so we can access log file
+            loggerFactory.Dispose();
+
+            AssertLogFileEntry(loggerSettings, LogLevel.Debug, msg, logFileName);
         }
     }
 }

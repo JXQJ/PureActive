d249a4245e35fc9c5e28f5d6d63f5a5bac8fb8bb
Steve Bush <stevebu@bushchang.com>
2018-11-01 08:35:27 -0700

    PureActive Archive Unit Tests

---
+++ test/PureActive.Archive.UnitTests/ArchiveUnitTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace PureActive.Archive.UnitTests
+{
+    public class ArchiveUnitTests
+    {
+        [Fact]
+        public void Archive_Create()
+        {
+
+        }
+    }
+}

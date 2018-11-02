0b46cc4b47d87666da0b92912d916d678febc3dd
Steve Bush <stevebu@bushchang.com>
2018-11-02 08:02:09 -0700

    Added Json Serialization support

---
+++ src/PureActive.Core/Serialization/TypeMapCollection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using PureActive.Core.Abstractions.Serialization;
+
+namespace PureActive.Core.Serialization
+{
+    /// <summary>
+    ///     A collection of type maps for the JSON serializer.
+    /// </summary>
+    public class TypeMapCollection : Dictionary<Type, IReadOnlyDictionary<string, Type>>, ITypeMapCollection
+    {
+    }
+}

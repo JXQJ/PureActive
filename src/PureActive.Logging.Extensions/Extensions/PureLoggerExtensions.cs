f7408da41cf9919aed16c1b59ac5e75ea45c9882
Steve Bush <stevebu@bushchang.com>
2018-11-05 17:25:48 -0800

    Refactoring of LoggerExtensions

--- src/PureActive.Logging.Extensions/Extensions/PureLoggerExtensions.cs
+++ src/PureActive.Logging.Extensions/Extensions/PureLoggerExtensions.cs
@@ -1,7 +1,15 @@
-﻿namespace PureActive.Logging.Extensions.Extensions
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+using PureActive.Logging.Abstractions.Interfaces;
+
+namespace PureActive.Logging.Extensions.Extensions
 {
     public static class PureLoggerExtensions
     {
-
+        public static IDisposable BeginPropertyScope<T>(this IPureLogger logger, string propertyName, T value)
+        {
+            return logger?.BeginScope(new Dictionary<string, T> { { propertyName, value } });
+        }
     }
 }

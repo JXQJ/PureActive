d28da4a40476475dbad1f99f4e5492e48c29b1d2
Steve Bush <stevebu@bushchang.com>
2018-11-15 10:37:21 -0800

    StringExtensionsUnitTests Part 1

--- src/PureActive.Core/Extensions/StringExtensions.cs
+++ src/PureActive.Core/Extensions/StringExtensions.cs
@@ -14,48 +14,27 @@ namespace PureActive.Core.Extensions
         ///     Changes the first character of a string to lower-case,
         ///     if it is not already.
         /// </summary>
-        public static string ToCamelCase(this string str)
-        {
-            if (str.Length == 0)
-                return str;
-
-            return str.Substring(0, 1).ToLower() + str.Substring(1);
-        }
+        public static string ToCamelCase(this string str) => string.IsNullOrEmpty(str) ? str : str.Substring(0, 1).ToLower() + str.Substring(1);
 
         /// <summary>
         ///     Changes the first character of a string to upper-case,
         ///     if it is not already.
         /// </summary>
-        public static string ToPascalCase(this string str)
-        {
-            if (str.Length == 0)
-                return str;
-
-            return str.Substring(0, 1).ToUpper() + str.Substring(1);
-        }
+        public static string ToPascalCase(this string str) => string.IsNullOrEmpty(str) ? str : str.Substring(0, 1).ToUpper() + str.Substring(1);
 
         /// <summary>
         ///     Returns a version of the string with non-alpha-numeric
         ///     characters stripped out.
         /// </summary>
-        public static string ToAlphaNumeric(this string str)
-        {
-            return new string(str.Where(char.IsLetterOrDigit).ToArray());
-        }
+        public static string ToAlphaNumeric(this string str) => string.IsNullOrEmpty(str) ? str: new string(str.Where(char.IsLetterOrDigit).ToArray());
 
-        public static string RemoveWhitespace(this string str)
-        {
-            return new string(str.Where(c => !char.IsWhiteSpace(c)).ToArray());
-        }
+        public static string RemoveWhitespace(this string str) => string.IsNullOrEmpty(str) ? str : new string(str.Where(c => !char.IsWhiteSpace(c)).ToArray());
 
         /// <summary>
         ///     Returns a version of the string with non-numeric
         ///     characters stripped out.
         /// </summary>
-        public static string ToNumeric(this string str)
-        {
-            return new string(str.Where(char.IsDigit).ToArray());
-        }
+        public static string ToNumeric(this string str) => string.IsNullOrEmpty(str) ? str : new string(str.Where(char.IsDigit).ToArray());
 
 
         /// <summary>
